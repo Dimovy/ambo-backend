bf1e59b42e5fbb13cd8ce1da370669d6db8c6ecc
Dimovy <55894047+Dimovy@users.noreply.github.com>
2020-06-06 15:15:35 +0300

    delete extra

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,6 @@ import { Logger } from '@overnightjs/logger';
 import { MongooseHelper } from '../util/MongooseHelper';
 import {Config} from './config/Config';
 import {EventController} from './controllers/EventController';
-import { TargetController } from './controllers/TargetController';
-import { PlayerController } from './controllers/PlayerController';
 
 export class App extends Server {
 
@@ -26,9 +24,7 @@ export class App extends Server {
                 console.log(err);
             })
         const eventController = new EventController();
-        const targetController = new TargetController();
-        const playerController = new PlayerController();
-        super.addControllers([eventController,targetController,playerController]/*, optional router here*/);
+        super.addControllers([eventController]/*, optional router here*/);
     }
 
     public start(port: number): void {
