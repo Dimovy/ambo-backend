2f34114c09b42fd5e8e42c7bb799780729277158
Dimovy <55894047+Dimovy@users.noreply.github.com>
2020-07-09 15:47:16 +0300

    add new elements to access model

--- src/models/AccessModel.ts
+++ src/models/AccessModel.ts
@@ -5,6 +5,9 @@ export interface Access extends Document {
     role: string,
     avatar: string,
     achievements: string,
+    phoneNumber: string,
+    birthday: string,
+    fullName: string,
     university: string
 }
 
@@ -18,6 +21,18 @@ export const accessSchema: Schema = new Schema({
         required: true,
     },
     achievements: {
+        type: String,
+        required: false,
+    },
+    phoneNumber: {
+        type: String,
+        required: false,
+    },
+    birthday: {
+        type: String,
+        required: false,
+    },
+    fullName: {
         type: String,
         required: false,
     },
